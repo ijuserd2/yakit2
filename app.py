b92236fc8bbf1b9e03dcb05381d69166628f89b1
kardelen <kardelen8759.college@gmail.com>
2024-03-14 20:21:09 +0300

    update yapıldı

--- app.py
+++ app.py
@@ -2,10 +2,8 @@
 
 from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
-import os
 import requests
-import time
-import json
+
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
 url = "https://www.petrolofisi.com.tr/akaryakit-fiyatlari"
@@ -27,18 +25,20 @@ def indexHtml():
             else:
                 iller = request.form.get("iller")
                 ilkodu = str(iller)
+                print(iller)
                 if ilkodu == "":
                     text = "Tekrar deneyin."
                     return render_template('index.html', data = data, text=text)
                 else:
                     soup = BeautifulSoup(r.text, "html.parser")
                     fuel = soup.find('tr', {'data-disctrict-id': ilkodu})
+                    iladi = fuel["data-disctrict-name"]
                     fuelprice = fuel.find_all('span', {'class': 'with-tax'})
                     data[0] = fuelprice[0].text
                     data[1] = fuelprice[1].text
                     data[2] = fuelprice[2].text
                     data[3] = fuelprice[3].text
-                    return render_template('index.html', data = data, text=ilkodu)
+                    return render_template('index.html', data = data, text=iladi)
                 
             
 if __name__ == "__main__":
